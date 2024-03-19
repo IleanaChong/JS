6360d983b2a1329ef6915952d833616f0a3e1629
IleanaChong <146903294+IleanaChong@users.noreply.github.com>
2024-03-19 19:16:53 +0800

    Add files via upload

---
+++ Mygame/intro1.js
@@ -0,0 +1,29 @@
+  class intro1 extends Phaser.Scene {
+
+    constructor ()
+    {
+        super({ key: "intro1" });
+    }
+
+    preload() {
+      this.load.image('intro1', 'assets/intro1.png')
+  
+  }
+  
+  create () {
+      this.m1 = this.add.image(0, 0, 'intro1').setOrigin(0, 0).setScale(1);
+     
+      console.log("Welcome");
+    //   let map = this.make.tilemap({ key: "world" });
+  
+      var spaceDown = this.input.keyboard.addKey('SPACE');
+      
+      spaceDown.on('down', function(){
+      console.log("Spacebar pressed, go to instruction");
+      this.scene.start("instruction");
+      }, this );
+  
+  }
+    
+  }
+
